8ee51854d2cef3ae7ebb9ac0c53c157fd30179e5
Divya Madhav Patil <divya.201513207@vcet.edu.in>
2023-06-21 17:36:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.superfresh').src = anything;
+}
+function colorchange(color){
+    const circle =document.querySelector('.circle');
+    circle.style.background = color;
+}
